572290ce4eca25ab961b525a4abeea8be7ad0e01
vinhee <115960216+vinhee@users.noreply.github.com>
2023-04-22 20:55:59 +0800

    Question 5

--- Question5.java
+++ Question5.java
@@ -1,32 +1,35 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Question5
-{
-  public static void main(String[] args)
-  {
-    /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
-     * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
-     * Hint: Use a loop to get input. Use another 2 loops to find the mode
-     */
-     
-    Scanner in = new Scanner(System.in);
-    
-  }
+class Question5 {
+    public static void main(String[] args) {
+        List<Integer> integerList = new ArrayList<>(); 
+        Scanner in = new Scanner(System.in);
+        System.out.println();
+        int tries = in.nextInt(); /**number of times user enters number */
+        
+        while (tries > 0) {
+            System.out.println();
+            int num = in.nextInt();
+            integerList.add(num);
+            tries--;
+        }/** number of times to print for user input and storing in list*/
+        
+        int max = 0;
+        int mode = 0;
+        for (int i = 0; i < integerList.size(); i++) {
+            int count = 0;
+                for (int j = 0; j < integerList.size(); j++) {
+                    if (integerList.get(i) == integerList.get(j)){
+                    count++;
+                    }
+                }
+            if (count >= max){
+                max = count;
+                mode = integerList.get(i);
+            }
+        }
+        System.out.println(mode);
+    }
 }
