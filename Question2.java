71eeb950499c18cde43f42770d07a483221abee4
vinhee <115960216+vinhee@users.noreply.github.com>
2023-04-22 20:57:24 +0800

    Question 2

--- Question2.java
+++ Question2.java
@@ -1,18 +1,16 @@
 import java.util.Scanner;
 
-public class Question2
-{
-  public static void main(String[] args)
-  {
-    Scanner in = new Scanner(System.in);
+public class Question2 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
 
-    System.out.println(); /**Ask user for input */
-    double height = in.nextDouble();
+        System.out.println(); /**Ask user for input */
+        double height = in.nextDouble();
 
-    System.out.println();
-    double weight = in.nextDouble();
+        System.out.println();
+        double weight = in.nextDouble();
 
-    double bmi = weight / (height * height);
-    System.out.println(bmi);
-  }
+        double bmi = weight / (height * height);
+        System.out.println(bmi);
+    }
 }
