28fd6e8880250b8617975dee1be84ee280d0ade0
vinhee <115960216+vinhee@users.noreply.github.com>
2023-04-22 17:41:58 +0800

    Question 4

--- Question4.java
+++ Question4.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 class Question4 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.printIn();
-        double num = in.nextDouble;
+        System.out.println();
+        double num = in.nextDouble();
 
-        for (int i = 0; i < num; i++){
-            for (int j = 5; j > i; j--){
-                 System.out.printIn("*");
-            }
+        for(double i = num;i > 0; i--)
+        {
+           for(int j = 0; j < i; j++)
+           {
+               System.out.print("*");
+           }
+           System.out.println();
         }
     }
 }
